38ac5100cbc8a91b86f3b627e5bfebbf177cd179
Efe Ariaroo <efeariaroo@gmail.com>
2022-10-27 22:40:40 +0100

    Add sample code to post to crunchypawn api server.

--- src/routes/posts/+page.ts
+++ src/routes/posts/+page.ts
@@ -1,6 +1,25 @@
 import type { PageLoad } from './$types';
+import axios from 'axios'
 
-export const load: PageLoad = ({ fetch }) => {
+export const load: PageLoad = async ({ fetch }) => {
+  console.log('Inside load ...')
+
+  const headers = {
+    'content-type': 'application/json'
+  }
+
+  const postPayload: any = {
+    username: 'Ken' + Math.random(),
+    password: 'GrandMaster',
+  }
+  const apiUrl = 'https://crunchypawn-dev.fly.dev/api/signup/signup'
+
+  const response = await axios.post(apiUrl, postPayload, {
+    headers
+  })
+
+  console.log(response.data)
+
 	const fetchPosts = async () => {
 		const res = await fetch('/posts');
 		const data = await res.json();
